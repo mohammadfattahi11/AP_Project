d88b4172a7ef9d64c7e790d247e950d4c44df059
Alireza Mirhosseini <shapourdisco78@gmail.com>
2021-07-10 11:24:28 +0430

    first correct run

--- login.cpp
+++ login.cpp
@@ -1,15 +1,15 @@
 #include "login.h"
 #include "ui_login.h"
 #include<QLabel>
+#include<QMessageBox>
 #include"information.h"
-#include"farm.h"
-#include "game.h"
+#include"game.h"
+
 login::login(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::login)
 {
     ui->setupUi(this);
-    ui->label_7->setHidden(1);
 }
 
 login::~login()
@@ -19,27 +19,25 @@ login::~login()
 
 void login::on_pushButton_clicked()
 {
-if(ui->lineEdit->text().isEmpty()||ui->lineEdit_2->text().isEmpty()){
-    ui->label_7->setText("username or password is empty please fill both of them");
-    if(ui->label_7->isHidden()){
-        ui->label_7->setHidden(0);
+    if(ui->lineEdit->text().isEmpty()||ui->lineEdit_2->text().isEmpty()){
+       QMessageBox::warning(this," ","username or password is empty please fill both of them");
     }
-}
-else{
-QJsonObject _info=read_info();
-  QJsonArray info = _info["User"].toArray();
-  for(int counter=0;counter<info.size();counter++){
-     if( (info[counter].toObject())["username"]==ui->lineEdit->text()&&(info[counter].toObject())["password"]==ui->lineEdit_2->text()){
-    this->close();
-     farm *w=new farm;
-     w->show();
-   }
-ui->label_7->setText("there is no any account with these username and password");
-if(ui->label_7->isHidden()){
-    ui->label_7->setHidden(0);
-}
-}
-}
+    else{
+        bool temp=1;
+        QJsonObject _info=read_info();
+        QJsonArray info = _info["User"].toArray();
+        for(int counter = 0; counter < info.size(); counter++){
+            if( (info[counter].toObject())["username"]==ui->lineEdit->text() && (info[counter].toObject())["password"] == ui->lineEdit_2->text()){
+                this->close();
+                temp = 0;
+                farm *w = new farm;
+                w->show();
+            }
+        }
+        if(temp){
+          QMessageBox::warning(this," ","there is no any account with these username and password");
+        }
+    }
 }
 
 void login::on_pushButton_2_clicked()
--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
   //  QMediaPlaylist *back_ground_sound=new
-    farm w;
-  //  login w;
+    //farm w;
+    login w;
     w.show();
     return a.exec();
 }
